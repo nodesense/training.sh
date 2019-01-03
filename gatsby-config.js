16501c25989752fabb42b5a5319317879b4ff2d4
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2019-01-03 12:33:34 +0530

    added mermaid, drawing tool

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,16 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+           {
+            resolve: 'gatsby-remark-graph',
+            options: {
+              // this is the language in your code-block that triggers mermaid parsing
+              language: 'mermaid', // default
+              theme: 'default' // could also be dark, forest, or neutral
+            }
+          },
+ 
+          
           {
             resolve: `gatsby-remark-images`,
             options: {
@@ -62,6 +72,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
